aededac13f8826e3087f52e21f47b9b8cd053727
chik <ochkurov96@gmail.com>
2023-05-15 15:38:59 +0300

    add Validaions

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,8 +1,17 @@
 const Router = require('express')
 const router = new Router()
 const contactController = require('../controllers/contact.controller')
+const {body} = require("express-validator");
 
-router.post('/contact', contactController.createContactinfo)
+const emailValidation = body('email').isEmail()
+const phoneValidatin = body('phone').isString().custom((value) => {
+    if(value.includes('+375')) {
+        return true
+    }
+    throw new Error('incorrect number')
+})
+
+router.post('/contact', emailValidation, phoneValidatin,contactController.createContactinfo)
 router.get('/contact', contactController.getContactInfoByUser)
 
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,9 @@ const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/user.controller')
 
+
+
+
 router.post('/user', userController.createUser)
 router.get('/user', userController.getUser)
 router.get('/user/:id', userController.getOneUser)
